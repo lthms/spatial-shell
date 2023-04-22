452d08c81025a8847798107d59e8b83aea2456db
Thomas Letan <lthms@soap.coffee>
2023-04-22 17:34:29 +0200

    Prepare the introduction of various way to output spatialmsg results

--- bin/spatialmsg/main.ml
+++ bin/spatialmsg/main.ml
@@ -17,31 +17,16 @@ let icon_of info =
   | "emacs" -> Some ""
   | _ -> None
 
+type format = Waybar
+
 let workspace_icon workspace windows =
   List.assq_opt workspace windows |> function
   | Some info -> Option.value ~default:default_icon (icon_of info)
   | None -> empty_workspace_icon
 
-let () =
-  Clap.description "A client to communicate with a Spatial instance.";
-
-  let ty = Clap.default_string ~short:'t' ~last:true "run_command" in
-
-  match ty with
-  | "run_command" ->
-      let cmd = Clap.mandatory_string ~placeholder:"CMD" () in
-      Clap.close ();
-
-      let cmd = command_of_string_exn cmd in
-      let { success } = send_command (Run_command cmd) in
-      if not success then exit 1
-  | "get_windows" -> (
-      let cmd = Clap.optional_int ~placeholder:"INDEX" () in
-      Clap.close ();
-
-      let reply = send_command Get_windows in
-
-      match (cmd, reply.focus) with
+let output_get_windows reply index = function
+  | Waybar -> (
+      match (index, reply.focus) with
       | Some idx, Some focus when idx < List.length reply.windows ->
           let window = List.nth reply.windows idx in
           let name =
@@ -58,13 +43,10 @@ let () =
               let marker = if reply.focus = Some idx then "*" else "" in
               Format.printf "| %s%s%s | " marker info.app_id marker)
             reply.windows)
-  | "get_workspaces" -> (
-      let cmd = Clap.optional_int ~placeholder:"INDEX" () in
-      Clap.close ();
 
-      let reply = send_command Get_workspaces in
-
-      match cmd with
+let output_get_workspaces reply index = function
+  | Waybar -> (
+      match index with
       | Some i ->
           let cls = if i = reply.current then "focus" else "unfocused" in
           Format.(
@@ -79,10 +61,56 @@ let () =
                  (fun fmt k ->
                    Format.fprintf fmt "%d:%s" k (workspace_icon k reply.windows)))
               (List.init 6 (fun x -> x + 1))))
-  | "get_workspace_config" ->
-      let reply = send_command Get_workspace_config in
+
+let output_get_workspace_config reply = function
+  | Waybar ->
       Format.(
         printf "%s  %d"
           (if reply.layout = Maximize then "" else "")
           reply.column_count)
-  | _ -> exit 2
+
+let format_clap () =
+  Clap.(
+    flag_enum
+      ~section:
+        (section "FORMAT"
+           ~description:
+             "Specify the format spatialmsg will use to output the result of \
+              the RPC command.")
+      [ ([ "waybar" ], [], Waybar) ]
+      Waybar)
+
+let () =
+  Clap.description "A client to communicate with a Spatial instance.";
+
+  let format = format_clap () in
+
+  let ty =
+    Clap.default_string ~long:"type" ~short:'t' ~last:true "run_command"
+      ~description:"Specify the type of IPC message."
+  in
+
+  match ty with
+  | "run_command" ->
+      let cmd = Clap.mandatory_string ~placeholder:"CMD" () in
+      Clap.close ();
+      let cmd = command_of_string_exn cmd in
+      let { success } = send_command (Run_command cmd) in
+      if not success then exit 1
+  | "get_windows" ->
+      let cmd = Clap.optional_int ~placeholder:"INDEX" () in
+      Clap.close ();
+      let reply = send_command Get_windows in
+      output_get_windows reply cmd format
+  | "get_workspaces" ->
+      let cmd = Clap.optional_int ~placeholder:"INDEX" () in
+      Clap.close ();
+      let reply = send_command Get_workspaces in
+      output_get_workspaces reply cmd format
+  | "get_workspace_config" ->
+      let reply = send_command Get_workspace_config in
+      Clap.close ();
+      output_get_workspace_config reply format
+  | _ ->
+      Clap.close ();
+      exit 2
