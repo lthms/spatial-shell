16616c0cc33a002e9352392f74edde77e96dc586
Thomas Letan <lthms@soap.coffee>
2023-04-23 10:29:23 +0200

    Rewrite contrib script in python

---
+++ contrib/spatialbar.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import json
+
+def shell(cmd):
+    stream = os.popen(cmd)
+    return json.loads(stream.read())
+
+def icon(glyph):
+    return f'<span font="Font Awesome 6 Free">{glyph}</span>'
+
+def icon_of_window(window):
+    match window['app_id']:
+        case 'firefox':
+            return icon('')
+        case 'kitty':
+            return icon('')
+        case 'Slack':
+            return icon('')
+        case 'emacs':
+            return icon('')
+        case _:
+            return icon('')
+
+def icon_of_workspace(workspace):
+    if not workspace:
+        return icon('◯')
+    else:
+        return icon_of_window(workspace['focused_window'])
+
+cmd = sys.argv[1]
+
+match cmd:
+    case "config":
+        reply = shell('spatialmsg -t get_workspace_config')
+        layout = reply['layout']
+        column_count = reply['column_count']
+        layout = icon('') if layout == "maximize" else f'{icon("")} {column_count}'
+
+        print(layout)
+    case "workspace":
+        workspace_index = int(sys.argv[2])
+        reply = shell('spatialmsg -t get_workspaces')
+        workspace = next(filter(lambda w: w['index'] == workspace_index, reply['workspaces']), {})
+        is_focus = 'focus' if workspace_index == reply['focus'] else 'unfocus'
+
+        print(f'{icon_of_workspace(workspace)}\n{workspace_index}\n{is_focus}')
+    case "window":
+        window_index = int(sys.argv[2])
+        reply = shell('spatialmsg -t get_windows')
+        window = (reply['windows'][window_index:window_index + 1] or [{}])[0]
+        if window:
+            is_focus = 'focus' if reply['focus'] == window_index else 'unfocus'
+            name = window['app_id']
+
+            print(f'{icon_of_window(window)}\n{name}\n{is_focus}')
+    case _:
+        exit(1)
